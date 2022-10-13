bf8714ac43ee579d6c24399a90f6d937711d51ea
ferchoferfernny <fercho.fer.fernny@hotmail.com>
2022-10-13 17:10:25 -0500

    Agrega Nombre de Grupos

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJA5S1pztDHjRrsFE8STqW_1hPVnGRWA4",
+  authDomain: "sabana-hack-494a0.firebaseapp.com",
+  projectId: "sabana-hack-494a0",
+  storageBucket: "sabana-hack-494a0.appspot.com",
+  messagingSenderId: "705749258389",
+  appId: "1:705749258389:web:b1a26fbfb12077aab74b9c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
